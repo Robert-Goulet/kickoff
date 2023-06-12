466a959579327c8756566349ed35743ae3287f8d
Robert Goulet <73008123+Robert-Goulet@users.noreply.github.com>
2023-06-12 13:07:50 -0400

    Update kickoff.sh

--- kickoff.sh
+++ kickoff.sh
@@ -1,48 +1,57 @@
 #!/bin/bash
+
+echo "Output of Notable Hidden Dot Files and XDG Base Directories" 
+
 #.bashhistory: history of shell commands typed by user
-echo "Last 20 shell commands typed by user"
-tail -n 20 .bash_history
+echo "Last 30 shell commands typed by user"
+stat ~/.bash_history
+tail -n 30 ~/.bash_history
 
 #.ssh/: secure shell configuration, keys, and lists of known hosts visited
 echo "SSH configuration, keys, and list of known hosts visited"
-tail -n 20 .ssh/
-if [ -s .ssh/ ]; then
+stat ~/.ssh/
+tail -n 20 ~/.ssh/
+if [ -s ~/.ssh/ ]; then
   echo "The file is not empty"
 else
   echo "The file is empty"
 fi
 
 #.viminfo: search and command history, traces of vim-edited files
-echo "Last 20 vim-edited files"
-tail -n 20 .viminfo
-if [ -s .viminfo ]; then 
+echo "Last 30 vim-edited files"
+stat ~/.viminfo
+tail -n 30 ~/.viminfo
+if [ -s ~/.viminfo ]; then 
  echo "The file is not empty" 
 else
  echo "The file is empty"
 fi
 
 #wget-hsts: list of wget hosts visited with timestamps
-echo "Last 20 wget hosts visited"
-tail -n 20 .wget-hsts
-if [ -s .viminfo ]; then 
+echo "Last 30 wget hosts visited"
+stat ~/.wget-hsts
+tail -n 30 ~/.wget-hsts
+if [ -s ~/.wget-hsts ]; then 
  echo "The file is not empty"
 else
  echo "The file is empty"
 fi
 
 #.forward: files containing email addresses for auto forwarding
-echo "Last 20 files containing email addresses for auto-forwarding"
-tail -n 20 .forward
-if [ -s .forward ]; then 
+echo "Last 30 files containing email addresses for auto-forwarding"
+stat ~/.forward
+tail -n 30 ~/.forward
+if [ -s ~/.forward ]; then 
  echo "The file is not empty"
 else 
   echo "The file is empty"
 fi
  
 #.mozilla/: Firefox configuration, cookies, bookmarks, browsing history, and plug-ins
-echo "Last 20 Firefox configuration, cookies, bookmarks, browsing history, and plug-ins"
-tail -n 20 .mozilla/
-if [ -s .mozilla/ ]; then
+echo "Last 30 Firefox configuration, cookies, bookmarks, browsing history, and plug-ins"
+stat ~/.mozilla/
+tail -n 30 ~/.mozilla/
+if [ -s ~/.mozilla/ ]; then
  echo "The file is not empty"
 else
  echo "The file is empty"
@@ -50,8 +59,9 @@ fi
   
 #.john/: John the Ripper password-cracking history with discovered passwords
 echo "John the Ripper password-cracking history with discovered passwords"
-cat .john/
-if [ -s .john/ ]; then
+stat ~/.john/
+cat ~/.john/
+if [ -s ~/.john/ ]; then
  echo "The file is not empty"
 else
  echo "The file is empty"
@@ -59,8 +69,25 @@ fi
 
 #mimeapps.list: Default applications
 echo "Default Applications List (Limit: 50)"
-cat -n 50 mimeapps.list
+stat ~/.config/mimeapps.list
+cat -n 50 ~/.config/mimeapps.list
+
+#.apvlvinfo: Contains a history of PDFs viewed using the apvlv PDF viewer
+echo "History of PDFs viewed using the apvlv PDF viewer (Limit 30)"
+tail -n 30 ~/.apvlvinfo
+if [ -s ~/.apvlvinfo ] then
+  echo "The file is not empty"
+else
+  echo "The file is empty"
+fi
 
 ##Additional actions:
 #cat ~/.cache/ 
-#cat ~/.cache/
+#cat ~/.config/
+
+echo "Contents of ~/Downloads Directory"
+stat ~/Downloads
+ls -lah ~/Downloads
+
+
+
